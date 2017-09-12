09be99d415dcdb3433ba92fa003e77e5711f477a
yogendrasri <yogendrasri@gmail.com>
2017-09-12 17:35:21 +0530

    changed print message

--- src/cc/engeld/HelloWorld.java
+++ src/cc/engeld/HelloWorld.java
@@ -2,6 +2,6 @@ package cc.engeld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World, I'm using Travis-CI!");
+        System.out.println("Hello World test, I'm using Travis-CI!");
     }
 }
